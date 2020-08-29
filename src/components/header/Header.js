f61fc638f08ab8796a06d55f5d5837912c2ef224
davidpxn <xuanphuong97@gmail.com>
2020-08-29 21:49:56 +0000

    Add fonts and header

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Header.scss';
+
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Google</h1>
+      <h1 className="header__subtitle">Fréttir</h1>
+    </header>
+  );
+}
+
+
+export default Header;
