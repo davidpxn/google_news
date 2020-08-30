4a923e8f8f5dca5272d7f9d5cc39e20660a56b8d
davidpxn <xuanphuong97@gmail.com>
2020-08-30 20:42:10 +0000

    Item cards for tablet and desktop

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import ViewportContext, { DeviceTypeEnum } from '../../contexts/ViewportContext';
+
+import { ReactComponent as IconArrow } from '../../resources/svg/arrow.svg';
 import PlaceholderImage from '../../resources/images/placeholder.png';
 import './Card.scss';
 
@@ -11,11 +14,24 @@ function Card(props) {
     contextLink,
   } = props;
 
+  const { deviceType } = useContext(ViewportContext);
+
+
+  const titleElement = (<h2 className="card__title">{title}</h2>);
+
   return (
     <div className="card">
       <a className="card__link" href={contextLink} target="_blank" rel="noopener noreferrer">
-        <img className="card__img" src={imgLink || PlaceholderImage} alt={title} />
-        <h2 className="card__title">{title}</h2>
+        <div className="card__img-container">
+          <img className="card__img" src={imgLink || PlaceholderImage} alt={title} />
+          {deviceType === DeviceTypeEnum.DESKTOP && (
+            <React.Fragment>
+              {titleElement}
+              <IconArrow className="card__arrow"/>
+            </React.Fragment>  
+          )}
+        </div>
+        {deviceType !== DeviceTypeEnum.DESKTOP && titleElement}
       </a>
     </div>
   );
